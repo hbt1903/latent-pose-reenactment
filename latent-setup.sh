71c252cff30c3803b438d5fe8612545e6058ac33
hbt1903 <hbt19031@gmail.com>
2022-03-01 13:04:26 +0300

    Add files via upload

---
+++ latent-setup.sh
@@ -0,0 +1,51 @@
+pip3 install \
+    torch==1.5.1 \
+    torchvision==0.6.1
+
+pip3 install \
+    tensorboard==2.2.2 \
+    pandas==1.0.5 \
+    torchsummary==1.5.1 \
+    thop==0.0.31-2005241907 \
+    tqdm==4.47.0 \
+    scipy==1.5.1 \
+    opencv-python==4.3.0.36 \
+    imageio==2.9.0 \
+    imageio-ffmpeg==0.4.2 \
+    pymemcache==3.2.0 \
+    python-memcached==1.59 \
+    matplotlib==3.2.2 \
+    seaborn==0.10.1 \
+    huepy==1.2.1 \
+    albumentations==0.4.6 \
+    imgaug==0.4.0 \
+    PyYAML==5.3.1 \
+    munch==2.5.0 \
+    pickle5==0.0.11 \
+    jpeg4py==0.1.4 \
+    face-alignment==1.1.1 \
+    mxnet-cu101mkl==1.6.0.post0 \
+    scikit-learn==0.23.2
+
+pip3 install git+https://github.com/DmitryUlyanov/yamlenv.git
+pip3 install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" git+https://github.com/NVIDIA/apex.git
+pip3 install git+https://github.com/dniku/insightface.git@763060b2
+
+git clone https://github.com/shrubb/latent-pose-reenactment
+git clone https://github.com/shrubb/Graphonomy.git latent-pose-reenactment/utils/Graphonomy
+
+pip install --upgrade --no-cache-dir gdown
+
+gdown --no-cookies --id 1QIVDRgtc_Fkaz9M4kewLU-TAFwJm7fvT
+gdown --no-cookies --id 1YDZRDnrPdJYvyRhe_Ir-91OFR_TCwvgO
+gdown --no-cookies --id 1o7s95QCrhRGyomuJDp5uGF1rhFSrbBN9
+gdown --no-cookies --id 1_ia1zRNZbfozBLVy-iOxYxdWRhtGhmZ2
+
+mkdir latent-pose-reenactment/model
+mkdir latent-pose-reenactment/utils/Graphonomy/data/model
+
+mv latent-pose-release.pth latent-pose-reenactment/model/latent-pose-release.pth
+mv universal_trained.pth latent-pose-reenactment/utils/Graphonomy/data/model/universal_trained.pth
+mv vgg19-d01eb7cb.pth latent-pose-reenactment/criterions/common/vgg19-d01eb7cb.pth
+mv vgg_face_weights.pth latent-pose-reenactment/criterions/common/vgg_face_weights.pth
+mv images latent-pose-reenactment/
---
+++ meta_train.sh
@@ -0,0 +1,26 @@
+DATASET_ROOT="/content/latent-pose-reenactment"
+IDENTITY_NAME="messi"
+MAX_BATCH_SIZE=8             # pick the largest possible, start with 8 and decrease until it fits in VRAM
+CHECKPOINT_PATH="/content/latent-pose-reenactment/model/latent-pose-release.pth"
+OUTPUT_PATH="/content/latent-pose-reenactment/outputs/"       # a directory for outputs, will be created
+RUN_NAME="tony_hawk_take_1"  # give your run a name if you want
+
+# Important. See the note below
+TARGET_NUM_ITERATIONS=125
+
+# Don't change these
+NUM_IMAGES=`ls -1 "$DATASET_ROOT/images-cropped/$IDENTITY_NAME" | wc -l`
+BATCH_SIZE=$((NUM_IMAGES<MAX_BATCH_SIZE ? NUM_IMAGES : MAX_BATCH_SIZE))
+ITERATIONS_IN_EPOCH=$(( NUM_IMAGES / BATCH_SIZE ))
+
+cd latent-pose-reenactment
+mkdir -p $OUTPUT_PATH
+python3 train.py \
+    --config finetuning-base                 \
+    --checkpoint_path "$CHECKPOINT_PATH"     \
+    --data_root "$DATASET_ROOT"              \
+    --train_split_path "$IDENTITY_NAME"      \
+    --batch_size $BATCH_SIZE                 \
+    --num_epochs $(( (TARGET_NUM_ITERATIONS + ITERATIONS_IN_EPOCH - 1) / ITERATIONS_IN_EPOCH )) \
+    --experiments_dir "$OUTPUT_PATH"         \
+    --experiment_name "$RUN_NAME"
