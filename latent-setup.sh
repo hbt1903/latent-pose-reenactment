feba503379745df19fc61b9f9575044e39bcbb4f
hbt1903 <hbt19031@gmail.com>
2022-03-01 13:26:54 +0300

    Update latent-setup.sh

--- latent-setup.sh
+++ latent-setup.sh
@@ -49,6 +49,7 @@ mv universal_trained.pth latent-pose-reenactment/utils/Graphonomy/data/model/uni
 mv vgg19-d01eb7cb.pth latent-pose-reenactment/criterions/common/vgg19-d01eb7cb.pth
 mv vgg_face_weights.pth latent-pose-reenactment/criterions/common/vgg_face_weights.pth
 mv images latent-pose-reenactment/
+mv videos latent-pose-reenactment/
 mv inference_folder.py latent-pose-reenactment/utils/Graphonomy/exp/inference/
 rm latent-pose-reenactment/utils/preprocess_dataset.sh
 mv preprocess_dataset.sh latent-pose-reenactment/utils/
